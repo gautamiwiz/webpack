bcc894b7c87cd3e814f47ee4828e8981e58c3e2a
Gautam Sharma <gautam@iwiz.in>
2022-05-13 21:57:13 +0530

    add rename js filename

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "images/[hash][ext][query]",
+    filename: "gScript.js",
   },
   module: {
     rules: [
